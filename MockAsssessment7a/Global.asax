b0e727d383364dba509b6f53c5fed7c1a06aa772
Callista <theevenstarlight@gmail.com>
2019-06-09 14:46:37 -0400

    first commit and need help

---
+++ MockAsssessment7a/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MockAsssessment7a.MvcApplication" Language="C#" %>
---
+++ MockAsssessment7a/Models/Doughnut.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MockAsssessment7a.Models
+{
+    public class Doughnut
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int Calories { get; set; }
+        public String PhotoURL { get; set; }
+        public String[] Extras { get; set; }
+
+        public Doughnut()
+        {
+
+        }
+    }
+}
